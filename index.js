14fd154c5ccd88da2a5c1091339bce795ec6bd9b
bilal898 <bfhamid@gmail.com>
2019-12-04 10:42:02 +0500

    lesson 41. add user to mongo through google

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const keys = require('./config/keys')
+mongoose.connect(keys.mongoURI);
+require('./models/User')
+require('./services/passport')
+
+require('./routes/authRoutes')(app)
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there1'})
+    res.send({ hi: 'sss334'})
+})
+app.get('/google', (req, res) => {
+    res.send({ hi: 'google'})
 })
 
-const PORT = process.env.PORT
+
+
+
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT)
